a5f223e7b12b4ae2746dec055228587d1520c0e3
AlonMamia <alon5585@gmail.com>
2026-09-15 14:22:12 +0300

    fix/adjust-csrf-to-spa

--- src/main/java/com/alon/bookstore/shared/config/SecurityConfig.java
+++ src/main/java/com/alon/bookstore/shared/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -42,9 +43,10 @@ public class SecurityConfig {
         http
                 .cors(Customizer.withDefaults())
 
-                .csrf(csrf -> csrf
-                        .csrfTokenRepository(new HttpSessionCsrfTokenRepository())
-                )
+//                .csrf(csrf -> csrf
+//                        .csrfTokenRepository(new HttpSessionCsrfTokenRepository())
+//                )
+                .csrf(CsrfConfigurer::spa)
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
